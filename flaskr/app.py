dd6000e899d4f4917cffc9996d8620e33142cd82
laineyjeon <gikyoj@uw.edu>
2023-02-18 09:55:58 -0800

    added html file

--- flaskr/app.py
+++ flaskr/app.py
@@ -26,7 +26,7 @@ def create_user():
     if Flask.request.method == 'GET': 
         # TODO: make a form asking for name and ebt and email
         # TODO: might be easier not to include ebt in this step and have them fill it in on their own in /redeem
-        return """<h1>Enter your details</>"""
+        return render_template('nonexistent.html') # UPDATE THIS TO THE CORRESPONDING HTML FILE!!!!!!!
     elif Flask.request.method == 'POST':
 
         # get the attributes from the post request form
@@ -88,7 +88,7 @@ def create_user():
 # marketplace for logged in user
 @app.route("/marketplace/<user>", methods=['GET'])
 def marketplace(user=None):
-    if not user: return "Log in bitch"
+    if not user: return "Log in bitch" #UPDATE THIS
 
     # get the database element for the logged in user
     u = supabase.table('Users').select("*").eq("id", user).execute()
